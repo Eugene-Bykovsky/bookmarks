3a4b766aa40de4086d167a06158438118f0e7887
Evgenii Bykovskii <45427494+Eugene-Bykovsky@users.noreply.github.com>
2024-01-28 20:27:03 +0100

    fix bug in def user_login

--- account/views.py
+++ account/views.py
@@ -6,7 +6,7 @@ from .forms import LoginForm
 
 def user_login(request):
     if request.method == 'POST':
-        form = LoginForm
+        form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request,
